9b2bf7240cd4a540fc7195ddbc17f34a43ff72f8
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-10 20:30:11 +0100

    Log PagerAdapter method calls.

--- app/src/main/java/uk/me/jeremygreen/merging/MainActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/MainActivity.kt
@@ -1,6 +1,9 @@
 package uk.me.jeremygreen.merging
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
@@ -39,6 +42,7 @@ class MainActivity : FragmentActivity() {
 
         var photos: List<File>  by Delegates.observable(photoManager.photos) { _, old, new ->
             if (old != new) {
+                Log.d(TAG, "notfifyDataSetChanged()")
                 // This must be called whenever getCount() will return a new value.
                 notifyDataSetChanged()
             }
@@ -46,16 +50,20 @@ class MainActivity : FragmentActivity() {
 
         // From PhotoManager.ChangeListener
         override fun onPhotosChange(files: List<File>) {
+            Log.d(TAG, "onPhotosChange(${files.size} photos)")
             photos = files
         }
 
         // From PagerAdapter
         override fun getCount(): Int {
-            return photos.size + 1;
+            val count = photos.size + 1
+            Log.d(TAG, "getCount() = ${count}")
+            return count;
         }
 
         // From PagerAdapter
         override fun getItem(position: Int): Fragment {
+            Log.d(TAG, "getItem(${position})")
             if (position < photos.size) {
                 return ImageFragment.newInstance(photos[position])
             } else {
@@ -68,7 +76,21 @@ class MainActivity : FragmentActivity() {
             // Need to return POSITION_NONE in general, otherwise deleted photos aren't removed from the ViewPager.
             // TODO for efficiency, only return POSITION_NONE if necessary.
             // ...https://stackoverflow.com/questions/10849552/update-viewpager-dynamically/10852046#10852046
-            return FragmentStatePagerAdapter.POSITION_NONE
+            val position = FragmentStatePagerAdapter.POSITION_NONE
+            Log.d(TAG, "getItemPosition(${`object`}) = ${position}")
+            return position
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val item = super.instantiateItem(container, position)
+            Log.d(TAG, "instantiateItem(${position}) = ${item}")
+            return item
+        }
+
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            val result = super.isViewFromObject(view, `object`)
+            Log.d(TAG, "isViewFromObject(${view},${`object`}) = ${result}")
+            return result
         }
 
     } // ScreenPagerAdapter
