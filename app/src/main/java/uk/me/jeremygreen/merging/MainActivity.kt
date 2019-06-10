108e3b69f4618120ab9bcd9a66b29e6f250caac1
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-10 21:04:24 +0100

    More efficient recycling of Fragments in ViewPager.

--- app/src/main/java/uk/me/jeremygreen/merging/MainActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
+import java.lang.AssertionError
 import kotlin.properties.Delegates
 
 class MainActivity : FragmentActivity() {
@@ -43,10 +44,16 @@ class MainActivity : FragmentActivity() {
         var photos: List<File>  by Delegates.observable(photoManager.photos) { _, old, new ->
             if (old != new) {
                 Log.d(TAG, "notfifyDataSetChanged()")
+                // Could leave Fragments that haven't moved in the MutableSet, but
+                // it's easier to just remove them all, so that all the Fragments
+                // are recreated.
+                fragments.clear()
                 // This must be called whenever getCount() will return a new value.
                 notifyDataSetChanged()
             }
         }
+
+        private val fragments: MutableSet<Fragment> = mutableSetOf()
 
         // From PhotoManager.ChangeListener
         override fun onPhotosChange(files: List<File>) {
@@ -64,20 +71,28 @@ class MainActivity : FragmentActivity() {
         // From PagerAdapter
         override fun getItem(position: Int): Fragment {
             Log.d(TAG, "getItem(${position})")
+            val fragment: Fragment
             if (position < photos.size) {
-                return ImageFragment.newInstance(photos[position])
+                fragment = ImageFragment.newInstance(photos[position])
             } else {
-                return AddImageFragment()
+                fragment = AddImageFragment()
             }
+            if (!fragments.add(fragment)) {
+                throw AssertionError("already present: ${fragment}")
+            }
+            return fragment
         }
 
         // From PagerAdapter
-        override fun getItemPosition(`object`: Any): Int {
+        override fun getItemPosition(obj: Any): Int {
             // Need to return POSITION_NONE in general, otherwise deleted photos aren't removed from the ViewPager.
-            // TODO for efficiency, only return POSITION_NONE if necessary.
-            // ...https://stackoverflow.com/questions/10849552/update-viewpager-dynamically/10852046#10852046
-            val position = FragmentStatePagerAdapter.POSITION_NONE
-            Log.d(TAG, "getItemPosition(${`object`}) = ${position}")
+            val position: Int
+            if (obj is Fragment && fragments.contains(obj)) {
+                position = FragmentStatePagerAdapter.POSITION_UNCHANGED
+            } else {
+                position = FragmentStatePagerAdapter.POSITION_NONE
+            }
+            Log.d(TAG, "getItemPosition(${obj}) = ${position}")
             return position
         }
 
@@ -87,9 +102,9 @@ class MainActivity : FragmentActivity() {
             return item
         }
 
-        override fun isViewFromObject(view: View, `object`: Any): Boolean {
-            val result = super.isViewFromObject(view, `object`)
-            Log.d(TAG, "isViewFromObject(${view},${`object`}) = ${result}")
+        override fun isViewFromObject(view: View, obj: Any): Boolean {
+            val result = super.isViewFromObject(view, obj)
+            Log.d(TAG, "isViewFromObject(${view},${obj}) = ${result}")
             return result
         }
 
