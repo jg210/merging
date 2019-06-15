b3f6359ff5aa7460b8114433601afccbeb050e37
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-15 16:34:32 +0100

    Use coroutine to avoid calling findById(id) on main thread.

--- app/src/main/java/uk/me/jeremygreen/merging/ImageFragment.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/ImageFragment.kt
@@ -9,6 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.image_screen.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 
 class ImageFragment : ScreenFragment() {
@@ -24,7 +28,6 @@ class ImageFragment : ScreenFragment() {
     }
 
     val BUNDLE_KEY__IMAGE_ID = "imageId"
-    lateinit var image: Image
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,38 +36,31 @@ class ImageFragment : ScreenFragment() {
     ): View? {
         val bundle = arguments
         val imageId: Long = bundle!!.getLong(BUNDLE_KEY__IMAGE_ID)
-        image = imageViewModel.findById(imageId)
-        return inflater.inflate(R.layout.image_screen, container, false)
-    }
-
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?
-    ) {
-        super.onViewCreated(view, savedInstanceState)
-        imageDraweeView.setOnLongClickListener {
-            handleLongClick()
-            false // not consumed
+        val view = inflater.inflate(R.layout.image_screen, container, false)
+        GlobalScope.launch(Dispatchers.IO) {
+            val image = imageViewModel.findById(imageId)
+            launch(Dispatchers.Main) {
+                val uri = Uri.fromFile(File(image.file))
+                imageDraweeView.setImageURI(uri, null)
+                imageDraweeView.setOnLongClickListener {
+                    handleLongClick(image)
+                    false // not consumed
+                }
+            }
         }
-        val uri = Uri.fromFile(File(image.file))
-        imageDraweeView.setImageURI(uri, null)
+        return view
     }
 
-    fun handleLongClick() {
+    fun handleLongClick(image: Image) {
         AlertDialog.Builder(requireContext()).apply {
             setMessage(R.string.confirmDeleteImage)
-            setPositiveButton(R.string.ok, DialogInterface.OnClickListener(function = ::handleRemoveImage))
+            setPositiveButton(R.string.ok, DialogInterface.OnClickListener(function = { _: DialogInterface, _: Int ->
+                imageDraweeView.setOnLongClickListener { false }
+                imageViewModel.delete(image)
+            }))
             setNegativeButton(R.string.cancel, DialogInterface.OnClickListener(function = ::handleRemoveImageCancel))
             show()
         }
-    }
-
-    fun handleRemoveImage(
-        @Suppress("UNUSED_PARAMETER") dialog: DialogInterface,
-        @Suppress("UNUSED_PARAMETER") which: Int
-    ) {
-        imageDraweeView.setOnLongClickListener { false }
-        imageViewModel.delete(image)
     }
 
     fun handleRemoveImageCancel(
