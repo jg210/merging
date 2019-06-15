fc9cd16db167459e76a2d529cfda255c167bda18
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-15 19:15:20 +0100

    Fix javadoc.

--- app/src/main/java/uk/me/jeremygreen/merging/Image.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/Image.kt
@@ -13,17 +13,14 @@ import java.io.File
 @Entity(tableName = "images")
 data class Image(
     /**
-     * The id is used to persist the image to storage. Images can be deleted, so there can be
-     * gaps in the ids. It is generally not the same as the position of the image within the UI.
+     * This database table stores id -> file mappings. It's not possible to
+     * just use the set of stored files since the UI can still get updates
+     * after deciding to delete the image but before it has been removed
+     * from the screen.
      *
-     * Use autoGenerate=true since:
-     *
-     * Rely on ordering by primary key to time order the photos. Could use UTC timestamp instead, but can't rely on it being
-     * correct).
-     *
-     * Reusing the same id causes problems for image deletion. If an image
-     * is deleted and added and the id reused, a UI that is still getting updated
-     * can get confused.
+     * Use autoGenerate=true since rely on ordering by primary key
+     * to time order the photos. Could use UTC timestamp instead,
+     * but can't rely on it being correct).
      */
     @PrimaryKey(autoGenerate = true)
     val id: Long,
