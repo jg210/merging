78a9adcf83b51617dfd691e3faae769b5211d47c
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-16 11:50:12 +0100

    Attempt to configure androidx Toolbar. supportActionBar and fab Views are null in onCreate though, and don't see a Toolbar or FAB.

--- app/src/main/java/uk/me/jeremygreen/merging/AddImageFragment.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/AddImageFragment.kt
@@ -1,105 +1,19 @@
 package uk.me.jeremygreen.merging
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.FileProvider
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
-import kotlinx.android.synthetic.main.add_image_screen.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.io.File
-import java.util.*
 
 class AddImageFragment : ScreenFragment() {
 
     private val TAG = "AddImageFragment"
-    private val REQUEST_TAKE_PHOTO = 1
-    private val BUNDLE_KEY__FILE = "file"
-    private var file: File? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState != null) {
-            val fileString = savedInstanceState.getString(BUNDLE_KEY__FILE)
-            if (fileString != null) {
-                file = File(fileString)
-            }
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.add_image_screen, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        addImageButton.setOnClickListener { handleTakePhoto() }
-        appIcon.setOnClickListener {
-            val intent: Intent = Intent(
-                requireActivity().applicationContext,
-                OssLicensesMenuActivity::class.java)   //Intent(this, OssLicensesMenuActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val file = this.file
-        if (file != null) {
-            outState.putString(BUNDLE_KEY__FILE, file.path)
-        }
-    }
-
-    private fun handleTakePhoto() {
-        val context = requireContext()
-        val intent = createTakePhotoIntent(context)
-        if (intent == null) {
-            Log.w(TAG, "No camera application available.")
-        } else {
-            startActivityForResult(intent, REQUEST_TAKE_PHOTO)
-        }
-    }
-
-    private fun createTakePhotoIntent(context: Context): Intent? {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        val cameraActivity = intent.resolveActivity(context.packageManager)
-        if (cameraActivity == null) {
-            return null
-        }
-        val uuid = UUID.randomUUID()
-        val file = File(imagesDir, "${uuid}.jpg")
-        val imageUri: Uri = FileProvider.getUriForFile(
-            context,
-            "uk.me.jeremygreen.merging.fileprovider",
-            file
-        )
-        this.file = file
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-        return intent
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_TAKE_PHOTO) {
-            val file = file
-            if (file == null) {
-                throw AssertionError()
-            }
-            GlobalScope.launch(Dispatchers.IO) {
-                imageViewModel.addImage(file.path)
-            }
-        }
     }
 
 }
--- app/src/main/java/uk/me/jeremygreen/merging/MainActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/MainActivity.kt
@@ -1,16 +1,54 @@
 package uk.me.jeremygreen.merging
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.File
+import java.util.*
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun imagesDir(activity: Activity): File {
+            return File(activity.filesDir, "photos")
+        }
+    }
+
+    private val TAG = "MainActivity"
+    private val REQUEST_TAKE_PHOTO = 1
+    private val BUNDLE_KEY__FILE = "file"
+    private var file: File? = null
+
+    private val imageViewModel by lazy {
+        ViewModelProviders.of(this).get(ImageViewModel::class.java)
+    }
+
+    val imagesDir: File by lazy {
+        MainActivity.imagesDir(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            val fileString = savedInstanceState.getString(BUNDLE_KEY__FILE)
+            if (fileString != null) {
+                file = File(fileString)
+            }
+        }
         setContentView(R.layout.content_main)
+        setSupportActionBar(toolbar)
         val pagerAdapter = PagerAdapterImpl(this)
         pager.adapter = pagerAdapter
         pager.offscreenPageLimit = 2
@@ -18,6 +56,62 @@ class MainActivity : FragmentActivity() {
         imageViewModel.allImages().observe(this, Observer { images ->
             pagerAdapter.setImages(images)
         })
+        supportActionBar!!.show()
+//        fab.setOnClickListener { handleTakePhoto() }
+//        appIcon.setOnClickListener {
+//            val intent: Intent = Intent(
+//                requireActivity().applicationContext,
+//                OssLicensesMenuActivity::class.java)   //Intent(this, OssLicensesMenuActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val file = this.file
+        if (file != null) {
+            outState.putString(BUNDLE_KEY__FILE, file.path)
+        }
+    }
+
+    private fun handleTakePhoto() {
+        val intent = createTakePhotoIntent()
+        if (intent == null) {
+            Log.w(TAG, "No camera application available.")
+        } else {
+            startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+        }
+    }
+
+    private fun createTakePhotoIntent(): Intent? {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val cameraActivity = intent.resolveActivity(baseContext.packageManager)
+        if (cameraActivity == null) {
+            return null
+        }
+        val uuid = UUID.randomUUID()
+        val file = File(imagesDir, "${uuid}.jpg")
+        val imageUri: Uri = FileProvider.getUriForFile(
+            baseContext,
+            "uk.me.jeremygreen.merging.fileprovider",
+            file
+        )
+        this.file = file
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        return intent
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_TAKE_PHOTO) {
+            val file = file
+            if (file == null) {
+                throw AssertionError()
+            }
+            GlobalScope.launch(Dispatchers.IO) {
+                imageViewModel.addImage(file.path)
+            }
+        }
     }
 
 } // MainActivity
