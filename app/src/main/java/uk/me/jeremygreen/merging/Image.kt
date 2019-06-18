f7a39d4c2d9f0034728dd7b2f395cb9c10fa2738
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-18 22:30:43 +0100

    KDoc formatting.

--- app/src/main/java/uk/me/jeremygreen/merging/Image.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/Image.kt
@@ -5,15 +5,16 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "images")
 data class Image(
+
     /**
      * This database table stores id -> file mappings. It's not possible to
      * just use the set of stored files since the UI can still get updates
-     * after deciding to delete the image but before it has been removed
+     * after deciding to delete the image, but before it has been removed
      * from the screen.
-     *
+     * 
      * Use autoGenerate=true since rely on ordering by primary key
      * to time order the photos. Could use UTC timestamp instead,
-     * but can't rely on it being correct).
+     * but can't rely on it being correct.
      */
     @PrimaryKey(autoGenerate = true)
     val id: Long,
