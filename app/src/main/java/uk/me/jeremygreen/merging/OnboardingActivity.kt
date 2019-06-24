e8d845d8b4e5685cff7f98b0c9c22febff911cd3
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-06-24 21:35:47 +0100

    Unregister click listener in OnboardingActivity.

--- app/src/main/java/uk/me/jeremygreen/merging/OnboardingActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/OnboardingActivity.kt
@@ -7,16 +7,29 @@ import kotlinx.android.synthetic.main.onboarding.*
 
 class OnboardingActivity: AppCompatActivity() {
 
+    // Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding)
         setSupportActionBar(onboardingToolbar)
+    }
+
+    // Activity
+    override fun onResume() {
+        super.onResume()
         onboarding_accept.setOnClickListener {
+            onboarding_accept.setOnClickListener(null)
             val intent = Intent(this, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
             startActivity(intent)
             finish()
         }
+    }
+
+    // Activity
+    override fun onPause() {
+        onboarding_accept.setOnClickListener(null)
+        super.onPause()
     }
 
 }
