900b399a9eecc8c78e7408463fcbd96e6a49b83b
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-07-01 18:56:08 +0100

    Ignore onboarding table update if version already accepted.

--- app/src/main/java/uk/me/jeremygreen/merging/model/OnboardingDao.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/model/OnboardingDao.kt
@@ -1,10 +1,7 @@
 package uk.me.jeremygreen.merging.model
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface OnboardingDao {
@@ -12,7 +9,7 @@ interface OnboardingDao {
     @Query("SELECT * from onboarding`` WHERE version = :version")
     suspend fun findById(version: Long): Onboarding?
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun add(version: Onboarding)
 
 }
