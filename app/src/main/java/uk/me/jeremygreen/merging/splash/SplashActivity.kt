3013101eb53705e7cf5a34a5edcbba419549e658
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-07-01 18:27:42 +0100

    Show splashscreen for at least 1.5s.

--- app/src/main/java/uk/me/jeremygreen/merging/splash/SplashActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/splash/SplashActivity.kt
@@ -1,22 +1,14 @@
 package uk.me.jeremygreen.merging.splash
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.style.BulletSpan
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.onboarding.*
 import kotlinx.coroutines.*
 import uk.me.jeremygreen.merging.main.MainActivity
 import uk.me.jeremygreen.merging.R
 import uk.me.jeremygreen.merging.model.ImageViewModel
 import uk.me.jeremygreen.merging.onboarding.OnboardingActivity
-import kotlin.coroutines.CoroutineContext
 
 class SplashActivity: AppCompatActivity(), CoroutineScope by MainScope() {
 
@@ -26,11 +18,19 @@ class SplashActivity: AppCompatActivity(), CoroutineScope by MainScope() {
         setContentView(R.layout.splash)
         val imageViewModel = ViewModelProviders.of(this).get(ImageViewModel::class.java)
         launch(Dispatchers.IO) {
-            val activity = if (imageViewModel.onboardingAccepted(OnboardingActivity.version)) {
+            val acceptedVersion = async {
+                imageViewModel.onboardingAccepted(OnboardingActivity.version)
+            }
+            val delayJob = launch {
+                // Show splash screen for at least this long.
+                delay(1500)
+            }
+            val activity = if (acceptedVersion.await()) {
                 MainActivity::class.java
             } else {
                 OnboardingActivity::class.java
             }
+            delayJob.join()
             val intent = Intent(this@SplashActivity, activity)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
             startActivity(intent)
