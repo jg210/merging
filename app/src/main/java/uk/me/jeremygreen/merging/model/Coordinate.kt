d3839c73d5ae5423252ab6fbf47a5d9cb1d2e098
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-07-25 20:51:46 +0100

    Add indexes to match foreign key constraints. Avoids the following build warning:
    warning: column references a foreign key but it is not part of an index. This may trigger full table scans whenever parent table is modified so you are highly advised to create an index that covers this column.

--- app/src/main/java/uk/me/jeremygreen/merging/model/Coordinate.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/model/Coordinate.kt
@@ -2,6 +2,7 @@ package uk.me.jeremygreen.merging.model
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -13,7 +14,8 @@ import androidx.room.PrimaryKey
             childColumns = ["faceId"],
             onDelete = ForeignKey.CASCADE
         )
-    ]
+    ],
+    indices = [Index(value = ["faceId"])]
 )
 data class Coordinate(
     @PrimaryKey(autoGenerate = true)
--- app/src/main/java/uk/me/jeremygreen/merging/model/FaceEntity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/model/FaceEntity.kt
@@ -2,6 +2,7 @@ package uk.me.jeremygreen.merging.model
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -13,7 +14,8 @@ import androidx.room.PrimaryKey
             childColumns = ["imageId"],
             onDelete = ForeignKey.CASCADE
         )
-    ]
+    ],
+    indices = [Index(value = ["imageId"])]
 )
 data class FaceEntity(
 
