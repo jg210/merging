39ca679d6ec3fde41bd594f6f80dfb568920e095
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-05-07 11:01:19 +0100

    Remove unused imports.

--- app/src/main/java/uk/me/jeremygreen/merging/about/AboutActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/about/AboutActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,7 +22,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import uk.me.jeremygreen.merging.BuildConfig
 import uk.me.jeremygreen.merging.R
-import uk.me.jeremygreen.merging.databinding.AboutBinding
 
 internal class AboutActivity: AppCompatActivity() {
 
