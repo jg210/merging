d4c5725afa0c82b525d4b537e141f94a038837a7
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-06-05 11:21:36 +0100

    Centre align merged image text.

--- app/src/main/java/uk/me/jeremygreen/merging/main/screen/MergedImage.kt
+++ app/src/main/java/uk/me/jeremygreen/merging/main/screen/MergedImage.kt
@@ -1,7 +1,12 @@
 package uk.me.jeremygreen.merging.main.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import uk.me.jeremygreen.merging.R
 
@@ -10,5 +15,13 @@ import uk.me.jeremygreen.merging.R
 
 @Composable
 fun MergedImage() {
-    Text(text = stringResource(R.string.merged_image))
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.merged_image)
+        )
+    }
 }
