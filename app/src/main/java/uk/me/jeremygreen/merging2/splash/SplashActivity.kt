061992d3bcf721b5e5f9eb5b9339a7f00eb8bb58
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-09-05 21:48:22 +0100

    Remove XML SplashActivity UI, relying on similar default behaviour from android OS. Can't remove SplashActivity yet, since it starts MainActivity/OnboardingActivity.

--- app/src/main/java/uk/me/jeremygreen/merging2/splash/SplashActivity.kt
+++ app/src/main/java/uk/me/jeremygreen/merging2/splash/SplashActivity.kt
@@ -1,5 +1,6 @@
 package uk.me.jeremygreen.merging2.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,39 +9,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import uk.me.jeremygreen.merging2.main.MainActivity
-import uk.me.jeremygreen.merging2.databinding.SplashBinding
 import uk.me.jeremygreen.merging2.model.AppViewModel
 import uk.me.jeremygreen.merging2.onboarding.OnboardingActivity
 
+@SuppressLint("CustomSplashScreen")
 internal class SplashActivity: AppCompatActivity(), CoroutineScope by MainScope() {
-
-    companion object {
-        private const val SPLASH_SCREEN_MINIMUM_DURATION_MILLIS: Long = 1000
-    }
 
     // Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = SplashBinding.inflate(this.layoutInflater)
-        setContentView(binding.root)
+        // No UI!
         val appViewModel = AppViewModel.getInstance(this, application)
         launch(Dispatchers.IO) {
             val acceptedVersion = async {
                 appViewModel.onboardingAccepted(OnboardingActivity.VERSION)
-            }
-            val delayJob = launch {
-                // Show splash screen for at least this long.
-                delay(SPLASH_SCREEN_MINIMUM_DURATION_MILLIS)
             }
             val activity = if (acceptedVersion.await()) {
                 MainActivity::class.java
             } else {
                 OnboardingActivity::class.java
             }
-            delayJob.join()
             val intent = Intent(this@SplashActivity, activity)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
             startActivity(intent)
